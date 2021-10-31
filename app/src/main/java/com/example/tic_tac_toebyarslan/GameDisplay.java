23daa57a7478b9cf88adc23d11d87178b5b3e7be
Arslan <arslanme343@gmail.com>
2021-10-31 21:46:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tic_tac_toebyarslan/GameDisplay.java
@@ -0,0 +1,34 @@
+package com.example.tic_tac_toebyarslan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class GameDisplay extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_display);
+
+
+    }
+
+    public void playAgainButtonClick(View view) {
+    }
+    public void homeButtonClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/tic_tac_toebyarslan/TicTacToeBoard.java
@@ -0,0 +1,8 @@
+package com.example.tic_tac_toebyarslan;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
